bc9f4aec25b063d3d5a0270f60740de994134230
dshinin <dshinin@gmail.com>
2023-09-05 09:14:14 +0300

    lesson 4 tasks

---
+++ task27/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int summ = 0;
+
+while (number > 10 ) {
+    summ = summ + number % 10;
+    number = number / 10;
+} 
+
+summ = summ + number;
+
+Console.WriteLine("Сумма чисел: "+ summ);
---
+++ task29/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] array = new int[8];
+
+Console.Write("[ ");  
+for (int i = 0; i < 8; i++)
+{
+    if (i > 0)  Console.Write(", "); 
+    array[i] = new Random().Next(-100,100);
+    Console.Write(array[i]); 
+}
+Console.Write(" ]");  
